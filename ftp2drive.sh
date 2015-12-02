a9fa6815308604b06d82268742256958e42fcfb8
Alex Vorona <alex@vorona.com.ua>
2015-12-02 20:10:25 +0200

    misc fixed

--- ftp2drive.sh
+++ ftp2drive.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # REQUIREMENTS:
-# dd, blockdev, mbuffer, curl, pigz; md5sum on ftp server to check sums
-#USAGE: sh ftp2drive.sh /dev/sda sda-PID-md5sum-raw.txt ftp://user:pass@server/dir/
+# dd, blockdev, mbuffer, curl, pigz; md5sum on f[35;71;24Mtp server to check sums
+#USAGE: sh ftp2drive.sh /dev/sda sda-PID-md5sum.txt ftp://user:pass@server/dir/
 
 #apt-get install mbuffer curl pigz
 #yum  -y install mbuffer curl pigz util-linux coreutils
 
 DD=dd
-DD_OPTS="status=none"
+DD_OPTS="status=noxfer"
 MBUFFER=mbuffer
 
 CURL=curl
@@ -16,7 +16,7 @@ GZIP=pigz
 BLOCKDEV=blockdev
 
 DRIVE=$1
-HASHLIST_RAW=$2
+HASHLIST=$2
 URI=$3
 
 
@@ -40,18 +40,18 @@ MYPID=$$
 DB_LOG=$TMPDIR/db.$MYPID.log
 GB_LOG=$TMPDIR/gb.$MYPID.log
 
-$CURL -s -o $TMPDIR/$HASHLIST_RAW "$URI/$HASHLIST_RAW"
+$CURL -s -o $TMPDIR/$HASHLIST "$URI/$HASHLIST"
 
-for FN in `cat $TMPDIR/$HASHLIST_RAW| sort -r` ; do
-    FN_START=$(echo -n $FN|egrep  -o "[0-9]{10,}" | head -1)
-    SEEK=$[$FN_START/$DD_BS]
+for FN in `cat $TMPDIR/$HASHLIST| awk '{print $2}'` ; do
+    FN_START=$(echo -n $FN|egrep  -o "[0-9]{10,}" | head -1|sed 's/^0*//')
+    let SEEK=FN_START/DD_BS
     if [ "$SEEK" != 0 ]; then 
         SEEK_STR="seek=$SEEK"
     else
         SEEK_STR=""
     fi
     echo -n "$FN "
-    $CURL -s "$URI/$FN.gz" | $MBUFFER --md5 -q -m $GZIP_BUF -l $GB_LOG |$GZIP $GZIP_OPTS|$MBUFFER $SRC_MD5 -q -m $DRIVE_BUF -l $DB_LOG|$DD of=$DRIVE bs=$DD_BS $SEEK_STR $DD_OPTS
+    $CURL -s "$URI/$FN" | $MBUFFER --md5 -q -m $GZIP_BUF -l $GB_LOG |$GZIP $GZIP_OPTS|$MBUFFER $SRC_MD5 -q -m $DRIVE_BUF -l $DB_LOG|$DD of=$DRIVE bs=$DD_BS $SEEK_STR $DD_OPTS
 done
 
 
